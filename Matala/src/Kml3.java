c6fe0fffefcd621e233caa82cf9ad49cfd9a3dfd
giladrozmarin <33815204+giladrozmarin@users.noreply.github.com>
2017-12-10 01:08:21 +0200

    blat

--- Matala/src/Kml3.java
+++ Matala/src/Kml3.java
@@ -2,6 +2,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+
+import org.omg.Messaging.SyncScopeHelper;
+
 import de.micromata.opengis.kml.v_2_2_0.Document;
 import de.micromata.opengis.kml.v_2_2_0.Kml;
 import de.micromata.opengis.kml.v_2_2_0.TimeStamp;
@@ -16,23 +19,25 @@ public class Kml3 extends file2csv{
 		String DAsString = new Double(x).toString();
 		return DAsString;
 	}
-		public static void ToKml(ArrayList<WiFiList> macs){	
+		public static void ToKml(){	
 			final Kml kml = new Kml();
 			Document document = kml.createAndSetDocument();
 			
-			for (int i = 0; i < macs.size(); i++) {
+			for (int i = 1; i < blabla.size(); i++) {
+				for (int j = 0; j < blabla.get(i).getList().size(); j++) {
+				System.out.println(blabla.size());
 				TimeStamp t = new TimeStamp();
 				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-				t.setWhen("yyyy-MM-dd HH:mm:ss").format((blabla.get(25).getList().get(1).getTime())));
-				document.createAndAddPlacemark().withName(macs.get(i).getID()).withOpen(Boolean.TRUE)
-				.withDescription(" Mac: "+macs.get(i).getMAC()+
-			" Signal: "+macs.get(i).getRSSI()+" SSID: "+macs.get(i).getID()+" Frequency: "+macs.get(i).getChannel())
+				t.setWhen(sdf.format((blabla.get(i).getList().get(j).getTime())));
+				document.createAndAddPlacemark().withName(blabla.get(i).getList().get(j).getSSID()).withOpen(Boolean.TRUE)
+				.withDescription(" Mac: "+(blabla.get(i).getList().get(j).getMAC())+
+			" Signal: "+(blabla.get(i).getList().get(j).getSignal())+" SSID: "+(blabla.get(i).getList().get(j).getSSID())+" Frequency: "+(blabla.get(i).getList().get(j).getFrequency()))
 				.withTimePrimitive(t).createAndSetPoint().
-				addToCoordinates(macs.get(i).getCLitude(),macs.get(i).getCLatitude());
-			}
+				addToCoordinates((blabla.get(i).getAlt()),(blabla.get(i).getLat()));
+			}}
 
 			try {
-				kml.marshal(new File("C:\\Users\\Xcompany\\Documents\\CSV\\KML.kml"));
+				kml.marshal(new File("C:\\Users\\gilad22\\Desktop\\KML.kml"));
 				System.out.println("completed");
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
